63e503b5a43ce9bbe3a29321bc50bb21a2c90642
wyydMac <1955406026@qq.com>
2026-05-02 16:39:38 +0800

    主结构

---
+++ src/main/java/com/me/galchat/GalchatApplication.java
@@ -0,0 +1,13 @@
+package com.me.galchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/me/galchat/GalchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.galchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

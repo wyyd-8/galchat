2dac18aed3e7a5e305b48574912c1b790bbd511f
wyydMac <1955406026@qq.com>
2026-09-24 21:34:49 +0800

    fix: restore chat jump arrow and adjust delay task queue

--- src/main/java/com/me/galchat/config/MessageThreadPoolConfig.java
+++ src/main/java/com/me/galchat/config/MessageThreadPoolConfig.java
@@ -20,7 +20,7 @@ public class MessageThreadPoolConfig {
         // 最大线程数
         executor.setMaxPoolSize(8);
         // 队列大小
-        executor.setQueueCapacity(10);
+        executor.setQueueCapacity(8);
         // 线程名前缀
         executor.setThreadNamePrefix("delay-task-handler-");
         executor.setWaitForTasksToCompleteOnShutdown(false);
